2989cffc7f7962dd0a4c502486bad42443773cf1
baoMoiHiz <legenlien@gmail.com>
2025-02-01 22:29:52 +0700

    Add files via upload

---
+++ dangnhap.js
@@ -0,0 +1,19 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Kiểm tra trạng thái đăng nhập
+    const daDangNhap = localStorage.getItem("daDangNhap");
+
+    if (daDangNhap === "true") {
+        // Nếu đã đăng nhập, hiển thị nội dung chính
+        document.getElementById("khuVucChinh").style.display = "block";
+    } else {
+        // Nếu chưa đăng nhập, chuyển về trang đăng nhập
+        window.location.href = "dangnhap.html";
+    }
+
+    // Xử lý sự kiện Đăng Xuất
+    document.getElementById("nutDangXuat").addEventListener("click", function () {
+        localStorage.removeItem("daDangNhap"); // Xóa trạng thái đăng nhập
+        window.location.href = "dangnhap.html"; // Quay lại trang đăng nhập
+    });
+});
